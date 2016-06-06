9cb47c148c44a4b762856b01207274faf823c0ff
lichen-pivotal <lichen@pivotal.io>
2016-06-06 12:41:49 +0800

    Update gpdb-docker.sh to only start the specified container

--- gpdb-docker.sh
+++ gpdb-docker.sh
@@ -10,7 +10,7 @@ if [ $docker_machine -eq 1 ]; then
     docker-machine create --driver virtualbox default
 fi
 
-docker_machine_state=`docker-machine ls | awk '{print $4}' | grep -v STATE`
+docker_machine_state=`docker-machine ls | awk '{print $4}' | sed 1d`
 
 if [ $docker_machine_state = "Stopped" ]; then
     docker-machine start
@@ -18,16 +18,16 @@ fi
 
 eval "$(docker-machine env default)"
 
-container=`docker ps -a | awk '{print $1}' | wc -l`
+container_num=`docker ps -a | awk '{print $1}' | sed 1d | wc -l`
+image_id=`docker images | grep $2 | awk '{print $3}'`
+container_id=`docker ps -a | grep $image_id | awk '{print $1}'`
 
-if [ $container -eq 1 ]; then
+
+if [ $container_num -eq 0 ]; then
     docker-machine ssh default "sudo sed -i 's/^.*label.*/--label provider=virtualbox --insecure-registry 10.152.9.39:5000/g' /var/lib/boot2docker/profile"
     docker-machine restart
     docker run -i -d -p 5432:5432 -p $1:22 -p 28080:28080 -h mdw 10.152.9.39:5000/$2
 else
-    for i in $(docker ps -a | awk '{print $1}' | grep -v CONTAINER)
-    do
-        docker restart $i
-    done
+    docker restart $container_id
 fi
 
