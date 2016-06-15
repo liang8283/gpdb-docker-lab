7e7469933d3c6ea401f1f9a6195b4ef060957b9d
lichen-pivotal <lichen@pivotal.io>
2016-06-15 08:49:41 +0800

    Update gpdb_docker.sh

--- gpdb-docker.sh
+++ gpdb-docker.sh
@@ -5,29 +5,36 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
+## Create virtualbox if it does not exist on Mac.
 docker_machine=`docker-machine ls | wc -l`
 if [ $docker_machine -eq 1 ]; then
     docker-machine create --driver virtualbox default
 fi
 
+## Start the virualbox if it is stopped.
 docker_machine_state=`docker-machine ls | awk '{print $4}' | sed 1d`
 
 if [ $docker_machine_state = "Stopped" ]; then
     docker-machine start
 fi
 
+## Set the env variables to run Docker commands.
 eval "$(docker-machine env default)"
 
+## Need to update virtual box config file "/var/lib/boot2docker/profile" if it isthe first time to pull the image from registry.
 container_num=`docker ps -a | awk '{print $1}' | sed 1d | wc -l`
-image_id=`docker images | grep $2 | awk '{print $3}'`
-container_id=`docker ps -a | grep $image_id | awk '{print $1}'`
-
 
 if [ $container_num -eq 0 ]; then
     docker-machine ssh default "sudo sed -i 's/^.*label.*/--label provider=virtualbox --insecure-registry 10.152.9.39:5000/g' /var/lib/boot2docker/profile"
     docker-machine restart
-    docker run -i -d -p 5432:5432 -p $1:22 -p 28080:28080 -h mdw 10.152.9.39:5000/$2
-else
-    docker restart $container_id
 fi
 
+## If the image does not exist on local Mac, pull it from registry and run it. Otherwise, just restart the existing container.
+image_id=`docker images | grep $2 | awk '{print $3}'`
+
+if [ -z "$image_id" ]; then
+    docker run -i -d -p 5432:5432 -p $1:22 -p 28080:28080 -h mdw 10.152.9.39:5000/$2
+else
+    container_id=`docker ps -a | grep $image_id | awk '{print $1}'`
+    docker restart $container_id
+fi
