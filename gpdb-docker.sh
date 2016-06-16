596db946fc428dd5c597a640171d9ffcf633c810
lichen-pivotal <lichen@pivotal.io>
2016-06-16 10:57:31 +0800

    update gpdb-docker.sh to add instruction

--- gpdb-docker.sh
+++ gpdb-docker.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+## INSTRUCTION:
+##
+## 1. Download Docker Toolbox (https://www.docker.com/products/docker-toolbox) and install. Just click "Next" to complete the installation.
+##
+## 2. Download and run the gpdb-docker.sh (https://drive.google.com/open?id=0B0xXFikI5TCIcDRnOG02ZXlrZ0k) script on local Mac. It will take several minutes to pull the image from 10.152.9.39 and run it.
+##
+##    Images available: 
+##     1) gpdb_4382 
+##     2) gpdb_ccb
+##
+##    ./gpdb-docker.sh <port> <image>
+##
+##    For example: ./gpdb-docker.sh 4382 gpdb_4382
+##
+## 3. Access the gpdb env on local Mac.
+##
+##    ssh gpadmin@`docker-machine ip` -p <port specified in step 2>
+
 if [ $# -ne 2 ]; then
     echo "Usage: ./gpdb_docker.sh <port> <image>"
     exit 1
